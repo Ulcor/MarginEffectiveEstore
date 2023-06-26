da48db183e92f46268ac0741c3fc73f91c40ebee
Alex <balgalvalex@gmail.com>
2023-06-26 06:56:09 +0300

    added index column, fixed white spaces, added comments

--- core/table_creator.py
+++ core/table_creator.py
@@ -4,6 +4,10 @@ from datetime import datetime, timedelta, date
 import numpy as np
 import openpyxl
 
+"""
+This set of functions allows to quickly generate test datasets 
+"""
+
 def dataset_generator(columns, column_types, num_rows):
     dataset = {}
     for col, col_type in zip(columns, column_types):
@@ -76,6 +80,7 @@ def _generate_date(num_rows, start_date, end_date):
     return dates
 
 
+# Use case
 # Set up prerequisites
 columns = ['category', 'amount', 'sale_date']
 column_types = ['object', 'int64', 'datetime64[ns]']
@@ -83,9 +88,11 @@ column_types = ['object', 'int64', 'datetime64[ns]']
 # Generate an empty dataset with 1000 rows
 # dataset = dataset_generator(columns, column_types, 1000)
 
+# Create empty dataset
 dataset = create_dataframe(columns=columns)
 
-days = 600
+# Day count
+days = 700
 
 min_date = date(year=2021, month=6, day=1)
 min_date_format = min_date.strftime("%Y-%m-%d")
@@ -100,6 +107,8 @@ print(f'The max date is {max_date}')
 dataset['category'] = randomizer('object', days, 'milk')
 dataset['amount'] = randomizer('int64', days, 11, 29, 1)
 dataset['sale_date'] = randomizer('datetime64[ns]', days, min_date_format, max_date_format)
+dataset['id'] = dataset.index
 
-print(dataset.head())
+# Check our dataset and save it in appropriate format
+print(dataset.info())
 dataset.to_excel('categories.xlsx', index=False)
